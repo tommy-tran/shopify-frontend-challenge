3c61256fcc7674e6d6a206864c245ac8064011ed
tommy-tran <trant28@mcmaster.ca>
2018-05-04 22:21:41 -0400

    Updated for responsiveness, more functions added to flow

--- js/index.js
+++ js/index.js
@@ -1,43 +1,34 @@
 // For changing interest dropdown color after selection
-function changeColor(self) {
-   self.style.color = "black";
+function changeDropdownColor(self) {
+   self.style.color = "#000";
 }
 
 /*
    Form handling
 */
 
+var submittable = true;
+
 var submitHandler = function submitHandler(e) {
    e.preventDefault();
-   var formData = new FormData(e.target);
-   var error = document.querySelector(".form__error");
-   error.innerHTML = "";
-   var submitButton = document.querySelector(".form__button");
+   if (submittable) {
+      var formData = new FormData(e.target);
+      var error = document.querySelector(".form__error");
+      error.innerHTML = "";
 
-   if (!validateEmail(formData.get("email"))) {
-      error.innerHTML = "Please enter a valid email address";
-      return;
-   } else {
-      submitButton.value = "Submitting...";
-      setTimeout(function () {
-         console.log(formData.get("email"));
-         console.log(formData.get("interest"));
-         successfulSubmission();
-      }, 2000);
+      if (!validateEmail(formData.get("email"))) {
+         error.innerHTML = "Please enter a valid email address";
+         return;
+      } else {
+         var user = {
+            email: formData.get("email"),
+            interest: formData.get("interest")
+         };
+         sendSubscribeRequest(user);
+         submittable = false;
+      }
    }
-
-   // Remove original event listener, ignore next attempts
-   ignoreNextSubmit();
 };
-
-// Used to ignore successive form submissions in submitHandler
-function ignoreNextSubmit() {
-   var form = document.querySelector("form");
-   form.removeEventListener("submit", submitHandler);
-   form.addEventListener("submit", function (e) {
-      return e.preventDefault();
-   });
-}
 
 document.querySelector("form").addEventListener("submit", submitHandler);
 
@@ -46,7 +37,22 @@ function successfulSubmission() {
    document.querySelector(".form__success-msg").style.display = "block";
 }
 
-// Email validator taken from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+function unsuccessfulSubmission() {
+   console.log("Failed to sign user up!");
+}
+
+function sendSubscribeRequest(user) {
+   var submitButton = document.getElementById("submit-button");
+   submitButton.value = "Submitting...";
+   setTimeout(function () {
+      var success = true;
+      console.log("Email: " + user.email);
+      console.log("Interest: " + user.interest);
+      success ? successfulSubmission() : unsuccessfulSubmission();
+   }, 2000);
+}
+
+// Email validator (taken from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript)
 function validateEmail(email) {
    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
    return re.test(String(email).toLowerCase());
